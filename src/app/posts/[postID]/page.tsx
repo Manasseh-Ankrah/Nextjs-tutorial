a977d609cd3d683d1605cace3531b0862bcfaf8f
Manasseh Ankrah <solomonankrah24@gmail.com>
2024-05-21 22:59:45 +0000

    Added new routes

---
+++ src/app/posts/[postID]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function page() {
+  return <div>SINGLE POST PAGE </div>;
+}
+
+export default page;
---
+++ src/app/posts/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function page() {
+  return <div>POSTS PAGE</div>;
+}
+
+export default page;
